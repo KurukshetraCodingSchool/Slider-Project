e584ffd60fa8ee46cb7967d947ab7343afbab78b
Kurukshetra <kurukshetracodingschool@gmail.com>
2024-12-20 20:48:35 +0530

    Slider Project Complete

--- script.js
+++ script.js
@@ -3,14 +3,14 @@ var slider =  document.querySelectorAll(".slider")
 var conut = 0;
 
 
-slider.forEach((item) => {
-    if(conut===0){
+slider.forEach((item,index) => {
         item.addEventListener("click",()=>{
-            item.style.width = "500px"
-            conut++;
-            console.log(conut)
+            slider.forEach((elam,i)=>{
+                elam.style.width = i === index ? "700px" : "150px"
+            })       
         })
-    }
-    
-    
+
 })
+
+
+
