1401355e6e6e998878d7ae82464ca0bade7f3629
Kurukshetra <kurukshetracodingschool@gmail.com>
2024-12-20 20:48:57 +0530

    update

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 var slider =  document.querySelectorAll(".slider")
 
-var conut = 0;
-
-
 slider.forEach((item,index) => {
         item.addEventListener("click",()=>{
             slider.forEach((elam,i)=>{
